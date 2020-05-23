ab78f2d43db2c85e4464eb3f89624fef3dae02a1
Tin Vujasinović <tin.vujasinovic@info-novitas.com>
2020-05-23 13:40:36 +0200

    Initial app commit

---
+++ KNXcontrol/KNXcontrol/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using KNXcontrol.Services;
+using KNXcontrol.Views;
+
+namespace KNXcontrol
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
